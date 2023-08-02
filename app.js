ac11bc412a11dbc86829e6a571449ab6c6765e52
ttajakka <tuomas.tajakka@gmail.com>
2023-08-02 11:54:49 +0300

    add endpoint that should shut down backend

--- app.js
+++ app.js
@@ -12,12 +12,11 @@ app.get('/health', (req, res) => {
 
 app.get('/break', (req, res) => {
   console.log('post to /break') // eslint-disable-line no-console
-  var aaa = 7 // eslint-disable-line no-unused-vars
-  aaa = aaa[0][0]
   res.send('ok')
+  server.close()
 })
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
 })
